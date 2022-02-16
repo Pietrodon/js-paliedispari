04dc4aff20a171cea5e5e2b8d5d41ad90b6a90ee
Pietrodon <donvitopietro00@gmail.com>
2022-02-16 17:46:58 +0100

    creazione funzione per estrazione numero random e controllo se sia pari o dispari

--- app.js
+++ app.js
@@ -23,14 +23,31 @@
 // scelta da parte dell'utente di pari o dispari
 // l'utente inserisce un numero da 1 a 5 
 let userNumber = 0;
-userNumber = prompt('inserisci un numero da 1 a 5')
+userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
+console.log(userNumber, typeof userNumber)
 if (userNumber>5){
     alert('il numero inserito deve essere compreso tra 1 e 5')
 }
 // estrazione randomica di un numero per il pc
-function computerNumber (){
-    Math.random
-}
+function randomNumber (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+  }
+  
+let computerNumber = (randomNumber(1,5))
+console.log(computerNumber)
 // somma del numero inserito dall'utente e quello del computer
+let sum = (userNumber + computerNumber)
+console.log(sum)
 // verifica se il numero è pari o dispari
+let check = false;
+function verifica(sum){
+    if (sum % 2 === 0){
+        check = true
+    } 
+    return check;
+}
+let controllo = verifica(sum)
+console.log(check)
 // alert per comunicar il vincitore
